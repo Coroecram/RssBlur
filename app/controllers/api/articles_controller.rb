8fde10c4695abfe277502687697205d622c91394
Michael.Budnick <michaeldbudnick@gmail.com>
2015-11-19 19:37:03 -0500

    Solve nokogiri?

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,24 +3,30 @@ require 'rss'
 require 'simple-rss'
 require 'link_thumbnailer'
 
+
+# ruby_item.description
+# html_doc = Nokogiri::HTML(ruby_article.description)
+# html_doc.css('img')
+# html_doc.elements.to_s
 class Api::ArticlesController < ApplicationController
   PAGE_SIZE = 10
 
   def index
     @articles = []
     rss = RSS::Parser.parse(open(params[:url]))
-      debugger
     page = params[:page].to_i
     range = (page...page+PAGE_SIZE)
     range.each do |idx|
-      article = rss.items[idx]
-      @article = Article.find_by_url(article.link)
+      ruby_article = rss.items[idx]
+      article = LinkThumbnailer.generate(ruby_article.link, image_limit: 1, http_open_timeout: 2, image_stats: false)
+      debugger
+      @article = Article.find_by_url(ruby_article.link)
       if !@article
         # @article = Article.create!(url: article.link,
-        #                            title: article.title,
+        #                            title: thumblink.title,
         #                            summary: article.description,
-        #                            author: article.dc_creator,
-        #                            created_date: article.pubDate,
+        #                            author: ruby_article.dc_creator,
+        #                            created_date: ruby_article.pubDate,
         #                            website_id: params[:website_id])
       end
       @articles.push(@article)
