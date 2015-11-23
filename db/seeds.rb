1d13f46d54a286696c6a640278907918230aa151
Michael.Budnick <michaeldbudnick@gmail.com>
2015-11-23 10:46:04 -0500

    Remove seeds for heroku validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!([{
-               username: "User1",
-               email: "user@1.com",
-               password:"password",
-               password_confirmation:"password"
-            },
-            {
-               username: "User2",
-               email: "user@2.com",
-               password:"password",
-               password_confirmation:"password"
-            },
-            {
-               username: "User3",
-               email: "user@3.com",
-               password: "password",
-               password_confirmation:"password"
-            },
-            {
-              username: "Guest",
-              email: "7evEUIpk1O1ajjK9lhihQ@belieber.com",
-              password: "AqXPos8Nz04DPBiBd0BIjQ",
-              password_confirmation:"AqXPos8Nz04DPBiBd0BIjQ"
-            }])
+# User.create!([{
+#                username: "User1",
+#                email: "user@1.com",
+#                password:"password",
+#                password_confirmation:"password"
+#             },
+#             {
+#                username: "User2",
+#                email: "user@2.com",
+#                password:"password",
+#                password_confirmation:"password"
+#             },
+#             {
+#                username: "User3",
+#                email: "user@3.com",
+#                password: "password",
+#                password_confirmation:"password"
+#             },
+#             {
+#               username: "Guest",
+#               email: "7evEUIpk1O1ajjK9lhihQ@belieber.com",
+#               password: "AqXPos8Nz04DPBiBd0BIjQ",
+#               password_confirmation:"AqXPos8Nz04DPBiBd0BIjQ"
+#             }])
 
 Website.create!([{
                   name: "WIRED",
@@ -52,34 +52,34 @@ Website.create!([{
                   is_feed: true
                  }])
 
-UserWebsite.create!([{
-                      user_id: 1,
-                      website_id: 1,
-                      folder_id: 0
-                    },
-                    {
-                      user_id: 1,
-                      website_id: 2,
-                      folder_id: 0
-                    },
-                    {
-                      user_id: 1,
-                      website_id: 3,
-                      folder_id: 0
-                    },
-                        {
-                          user_id: 2,
-                          website_id: 1,
-                          folder_id: 0
-                        },
-                        {
-                          user_id: 2,
-                          website_id: 2,
-                          folder_id: 0
-                        },
-                            {
-                              user_id: 3,
-                              website_id: 3,
-                              folder_id: 0
-                            }
-                    ])
+# UserWebsite.create!([{
+#                       user_id: 1,
+#                       website_id: 1,
+#                       folder_id: 0
+#                     },
+#                     {
+#                       user_id: 1,
+#                       website_id: 2,
+#                       folder_id: 0
+#                     },
+#                     {
+#                       user_id: 1,
+#                       website_id: 3,
+#                       folder_id: 0
+#                     },
+#                         {
+#                           user_id: 2,
+#                           website_id: 1,
+#                           folder_id: 0
+#                         },
+#                         {
+#                           user_id: 2,
+#                           website_id: 2,
+#                           folder_id: 0
+#                         },
+#                             {
+#                               user_id: 3,
+#                               website_id: 3,
+#                               folder_id: 0
+#                             }
+#                     ])
