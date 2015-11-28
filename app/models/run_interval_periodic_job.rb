837bd8e4021de0a83fbeae7844333bf05057b3a6
michael.budnick <michael.budnick@outlook.com>
2015-11-28 12:26:38 -0500

    Fix syntax error

--- app/models/run_interval_periodic_job.rb
+++ app/models/run_interval_periodic_job.rb
@@ -1,7 +1,7 @@
 class RunIntervalPeriodicJob < PeriodicJob
   # RunIntervalPeriodicJobs run if PeriodicJob#last_run_at time plus  # PeriodicJob#interval (in seconds) is past the current time (Time.now).
   def self.find_all_need_to_run
-    self.find(:all).select {|job| job.last_run_at.nil? || job.last_run_at + job.interval <= Time.now.utc)}
+    self.find(:all).select {|job| job.last_run_at.nil? || (job.last_run_at + job.interval <= Time.now.utc)}
   end
 
   def job_to_run_on_interval
