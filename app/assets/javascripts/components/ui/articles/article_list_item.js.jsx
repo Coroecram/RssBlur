e31c5115d92753fde6c7de96ff7489df4b717b28
michael.budnick <michael.budnick@outlook.com>
2015-11-28 22:20:09 -0500

    Change styling of read article list LI

--- app/assets/javascripts/components/ui/articles/article_list_item.js.jsx
+++ app/assets/javascripts/components/ui/articles/article_list_item.js.jsx
@@ -3,8 +3,9 @@ var ArticleListItem = React.createClass({
   render: function () {
     var articleDate = new Date(this.props.article.created_date);
     var prettyDate = DateFormat.format.prettyDate(articleDate);
+    var readClass = (this.props.unread ? "listed-article" : "listed-article read")
     return (
-            <li className="listed-article"
+            <li className={readClass}
                 data-unread={this.props.unread}
                 data-index={this.props.index}
                 data-article-id={this.props.article.id}
