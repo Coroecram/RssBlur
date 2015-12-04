900d6c8c38e5a7c64f261ef50bece5c8f6404026
michael.budnick <michael.budnick@outlook.com>
2015-12-04 06:29:01 -0500

    Debug empty website_id

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -4,7 +4,8 @@ class Api::ArticlesController < ApplicationController
   before_action :require_user_website, only: :index
 
   def index
-    current_articles = CurrentArticleParser.new(params[:website_id].to_i, params[:url])
+    current_articles = CurrentArticleParser.new(current_user.id, 0,
+                                                params[:website_id].to_i, params[:url])
     @articles = current_articles.articles
     render json: @articles
   end
--- app/services/current_article_parser.rb
+++ app/services/current_article_parser.rb
@@ -5,11 +5,12 @@ require 'rss'
 class CurrentArticleParser
   attr_reader :articles
 
-  def initialize(website_id, url)
+  def initialize(current_user_id, page, website_id, url)
     @articles = []
-    @website_id = []
+    @current_user_id = current_user_id
+    @page = page
+    @website_id = website_id
     @url = url
-
     current_articles
   end
 
@@ -18,7 +19,7 @@ class CurrentArticleParser
                            .order(created_date: :desc)
     current_articles = current_articles.to_a.map(&:serializable_hash)
     current_articles_ids = current_articles.map{ |article| article["id"] }
-    user_articles = UserArticle.current_user_articles(current_articles_ids)
+    user_articles = UserArticle.current_user_articles(@current_user_id, current_articles_ids)
     user_articles = user_articles.to_a.map(&:serializable_hash)
     user_article_keys = {}
     user_articles.each { |user_article| user_article_keys[user_article["article_id"].to_i] = user_article }
@@ -27,19 +28,19 @@ class CurrentArticleParser
     current_articles.each { |article| current_article_created_keys[article["created_date"].to_i] = article }
     current_articles.each { |article| current_article_url_keys[article["url"]] = article }
 
-    return feed_parse(current_article_created_keys, current_article_url_keys)
+    return feed_parse(current_article_created_keys, current_article_url_keys, user_article_keys)
   end
 
   private
 
-  def feed_parse(current_articles_created_keys, current_article_url_keys)
+  def feed_parse(current_article_created_keys, current_article_url_keys, user_article_keys)
     begin
       rss = Feedjira::Feed.fetch_and_parse @url
     rescue
       rss = RSS::Parser.parse(@url, do_validate=false)
     end
-    page = params[:page].to_i
-    range = (page...page+rss.entries.length)
+    page = @page
+    range = (@page...@page+rss.entries.length)
     range.each do |idx|
       rss_article = rss.entries[idx]
       url, title, author, summary, image, created_date = article_parser(rss_article, rss)
@@ -57,12 +58,13 @@ class CurrentArticleParser
       new_article_id = next_article["id"] || next_article.id
       @articles.push(next_article)
       user_article_keys[new_article_id] || UserArticle.create!(
-                                                        user_id: current_user.id,
+                                                        user_id: @current_user_id,
                                                         article_id: new_article_id,
                                                         read: false,
                                                         website_id: @website_id
                                                       )
-      return @articles
+      end
+    return @articles
   end
 
   def article_parser(rss_entry, source)
