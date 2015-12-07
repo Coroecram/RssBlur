d559aa8fe19ef8bb40ea654967b90f710507f326
michael.budnick <michael.budnick@outlook.com>
2015-12-07 07:24:47 -0500

    Fix bug

--- app/assets/javascripts/components/ui/sidebar/header.js.jsx
+++ app/assets/javascripts/components/ui/sidebar/header.js.jsx
@@ -2,7 +2,8 @@ var Header = React.createClass({
     mixins: [ReactRouter.History],
 
   getInitialState: function () {
-    return {currentUser: CurrentUserStore.fetch()};
+    return {currentUser: CurrentUserStore.fetch(),
+            settings: false};
   },
 
   componentDidMount: function () {
@@ -20,15 +21,31 @@ var Header = React.createClass({
   _profileSettings: function () {
   },
 
+  _signOut: function () {
+
+  },
+
   render: function () {
     var username =  /Guest.{16}/.exec(this.state.currentUser.username) ?
                         "Guest" : this.state.currentUser.username;
+    var settings
+    if (this.state.settings) {
+      settings = (
+        <ul>
+          <li className="settings">Profile Settings</li>
+          <li className="settings" onClick={this._signOut}>Sign Out</li>
+        </ul>
+      );
+    }
     return (
           <div className="articles-header group">
             <div className="focus-header">
               {"Welcome, " + username}
             </div>
-            <div className="profile-settings"><i className="fa fa-cog"></i></div>
+            <div className="profile-settings" onclick={this._profileSettings}>
+              <i className="fa fa-cog"></i>
+            </div>
+            {settings}
           </div>
           );
   }
