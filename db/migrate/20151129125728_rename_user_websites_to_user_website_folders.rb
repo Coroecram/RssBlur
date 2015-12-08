1e78497c69031237bfd516ff340a41a1f343eb72
michael.budnick <michael.budnick@outlook.com>
2015-12-08 18:14:02 -0500

    No periodic jobs

--- db/migrate/20151129125728_rename_user_websites_to_user_website_folders.rb
+++ db/migrate/20151129125728_rename_user_websites_to_user_website_folders.rb
@@ -1,16 +1,13 @@
 class RenameUserWebsitesToUserWebsiteFolders < ActiveRecord::Migration
   def change
-    create_table :periodic_jobs do |t|
+    create_table :folders do |t|
       t.string :name
       t.integer :user_website_id
       t.boolean :root
 
       t.timestamps
     end
-    add_column(:folders, :user_website_id, :integer)
-    add_column(:folders, :root, :boolean, default: false)
-    add_column(:folders, :created_at, :datetime)
-    add_column(:folders, :updated_at, :datetime)
+
     add_column(:user_articles, :created_at, :datetime)
     add_column(:user_articles, :updated_at, :datetime)
 
