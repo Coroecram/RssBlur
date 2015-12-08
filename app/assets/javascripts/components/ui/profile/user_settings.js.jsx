a6b8ea93cf404a2bcbae1e5cc198fdc6e9acb97a
michael.budnick <michael.budnick@outlook.com>
2015-12-07 21:50:32 -0500

    Style user settings component

--- app/assets/javascripts/components/ui/profile/user_settings.js.jsx
+++ app/assets/javascripts/components/ui/profile/user_settings.js.jsx
@@ -9,30 +9,34 @@ render: function () {
   return (
           <div className="user-settings">
             <h1>User Settings</h1>
-            <div className="profile-pic">
-              <img className="large-thumb" />
-              <p>Current Avatar</p>
-              <input id="uploadBtn" type="file" class="upload" />
-            </div>
-            <h2>Change Password</h2>
             <form className="update-form" onSubmit={ this._changePassword }>
-              <label>Change Password
-              <br/>
-                <input type="password"
-                       name="password"
-                       maxLength="35"
-                       valueLink={this.linkState('password')} />
-                </label>
+            <div className="profile-pic subform group">
+                <h2>Current Avatar</h2>
+              <img className="large-thumb">
+              </img>
+              <input id="uploadBtn" type="file" className="upload" />
+              <div className="upload-button">Update Avatar</div>
+            </div>
+            <div className="subform">
+              <h2>Change Password</h2>
+                <label>New Password
                 <br/>
-              <label>Confirm Password
-              <br/>
-                <input type="password"
-                       name="password_confirmation"
-                       maxLength="35"
-                       valueLink={this.linkState('passwordConfirmation')} />
-                </label>
+                  <input type="password"
+                         name="password"
+                         maxLength="35"
+                         valueLink={this.linkState('password')} />
+                  </label>
+                  <br/>
+                <label>Confirm Password
                 <br/>
-              <input type="submit" value="Change Password" />
+                  <input type="password"
+                         name="password_confirmation"
+                         maxLength="35"
+                         valueLink={this.linkState('passwordConfirmation')} />
+                  </label>
+                  <br/>
+                <input type="submit" value="Change Password" />
+              </div>
             </form>
           </div>
         );
