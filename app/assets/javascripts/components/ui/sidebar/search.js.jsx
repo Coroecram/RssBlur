50f4dc0032c2426fc2d6065ce8606576b60fdbe0
michael.budnick <michael.budnick@outlook.com>
2015-12-10 20:07:05 -0500

    Add filter option components

--- app/assets/javascripts/components/ui/sidebar/search.js.jsx
+++ app/assets/javascripts/components/ui/sidebar/search.js.jsx
@@ -1,11 +1,18 @@
 var Search = React.createClass({
 
+  getInitialState: function () {
+    return {query: "", filter: "all"};
+  },
+
   search: function (event) {
     ArticleApiActions.search(event.currentTarget.value);
   },
 
   blankState: function () {
-    this.setState({query: ""});
+    this.setState({query: "", filter: "all"});
+  },
+
+  chooseFilter: function (event) {
   },
 
   componentWillReceiveProps: function () {
@@ -13,6 +20,7 @@ var Search = React.createClass({
   },
 
   render: function () {
+    var allActive = (this.state.)
       return (
               <div className="search-form">
                   <label htmlFor="query" >Search for Articles:</label>
@@ -20,6 +28,15 @@ var Search = React.createClass({
                              name="query"
                              onChange={ this.search }
                              id="website-url"/>
+                    <div className="filters">
+                      <p>Search By: </p>
+                      <div className="filter-choices" onClick={ this.chooseFilter }>
+                        <p className={allActive}>All</p>
+                        <p className={titleActive}>Title</p>
+                        <p className={summaryActive}>Summary</p>
+                        <p className={authorActive}>Author</p>
+                      </div>
+                    </div>
               </div>
             );
     }
