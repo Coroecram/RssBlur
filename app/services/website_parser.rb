1b00f8978c037560f187345988250da70d2ae395
michael.budnick <michael.budnick@outlook.com>
2016-01-31 14:55:42 -0500

    Fix npr double creation (need to find new metainspector workaround)

--- app/services/website_parser.rb
+++ app/services/website_parser.rb
@@ -22,6 +22,7 @@ class WebsiteParser
       url = page.feed || page.url
       url = "http://www.thenation.com/feed/?post_type=article" if page.url == "http://www.thenation.com/"
       url = "http://www.wired.com/feed/" if page.url == "http://www.wired.com/"
+      url = "http://www.npr.org/rss/rss.php?id=1001" if page.feed =~ /http:\/\/www\.npr\.org\/rss\/rss\.php.*/
       @website = Website.find_by_url(url)
       if @website
         UserWebsite.find_or_create_by(user_id: @user_id, website_id: @website.id)
@@ -33,7 +34,7 @@ class WebsiteParser
         root_page = MetaInspector.new(root_url)
         doc = Nokogiri::XML(open(root_url))
         begin
-          name = "#{doc.xpath("//title").children.first.text[0..10]} Feed"
+          name = root_page.title[0..10] || "#{doc.xpath("//title").children.first.text[0..10]} Feed"
         rescue
           name = "#{root_page.host[0..10]} Feed"
         end
