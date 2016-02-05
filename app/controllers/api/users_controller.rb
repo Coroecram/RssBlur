c6ff70cd0d7e0cc4e52572af138f68930d588caf
michael.budnick <michael.budnick@outlook.com>
2016-02-05 11:54:42 -0500

    Pass down correct signup message

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,11 +12,11 @@ class Api::UsersController < ApplicationController
 
   def create
    @user = User.new(user_params)
-   if @user
+   if @user.valid?
      sign_up!(@user)
      render :show
    else
-     render json: @user.errors.full_messages.to_sentence, status: 401
+     render json: @user.errors.full_messages, status: 401
    end
   end
 
