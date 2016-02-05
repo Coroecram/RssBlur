df061c989f50e1f886358a99de5a5082f26a216d
michael.budnick <michael.budnick@outlook.com>
2016-02-05 15:01:39 -0500

    Build out mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,13 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@rssblur.com"
+  layout 'welcome'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://www.rssblur.com'
+    @portfolio_url = 'http://www.mikebudnick.com'
+    @game_url = 'http://www.mikebudnick.com/bountyhunter'
+    @resume_url = 'https://docs.google.com/document/d/1Sb45_jBo4zbgZjSEX2gPGYV-SonLddQlQDGua-7Txy4/export?format=pdf'
+    mail(to: @user.email, subject: 'Welcome to RSSBlur!')
+  end
 end
