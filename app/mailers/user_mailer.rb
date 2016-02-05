7948313d920047705d96b2fe24694fe14ff30320
michael.budnick <michael.budnick@outlook.com>
2016-02-05 15:37:55 -0500

    Add resume attached to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
     @url  = 'http://www.rssblur.com'
     @portfolio_url = 'http://www.mikebudnick.com'
     @game_url = 'http://www.mikebudnick.com/bountyhunter'
-    @resume_url = 'https://docs.google.com/document/d/1Sb45_jBo4zbgZjSEX2gPGYV-SonLddQlQDGua-7Txy4/export?format=pdf'
     mail(to: @user.email, subject: 'Welcome to RSSBlur!')
+    attachments['MichaelBudnickResume.pdf'] = File.read('app/assets/pdfs/MichaelBudnickResume.pdf')
   end
 end
