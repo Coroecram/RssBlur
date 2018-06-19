1b0020cbaf9520529f809682a2152397520c2470
Michael Budnick <michaeldbudnick@gmail.com>
2018-06-19 17:26:17 -0400

    Simplify RSS feeds

--- app/services/website_parser.rb
+++ app/services/website_parser.rb
@@ -1,5 +1,5 @@
+require 'rss'
 require 'open-uri'
-require 'metainspector'
 
 class WebsiteParser
   attr_reader :website, :success
@@ -12,56 +12,34 @@ class WebsiteParser
   end
 
   def website_parse
-    begin
-      page = MetaInspector.new(@url, :allow_non_html_content => true)
-    rescue e
-      page = false
-    end
-    if page
-      feed = feed_validation(page)
-      url = page.feed || page.url
-      url = "http://www.wired.com/feed/" if page.url == "http://www.wired.com/"
-      url = "http://www.npr.org/rss/rss.php?id=1001" if page.feed =~ /http:\/\/www\.npr\.org\/rss\/rss\.php.*/
-      @website = Website.find_by_url(url)
-      if @website
-        UserWebsite.find_or_create_by(user_id: @user_id, website_id: @website.id)
-        @success = true
-        return
-      elsif feed
-        root_uri = URI(page.url)
-        root_url = "#{root_uri.scheme}://#{root_uri.host}"
-        root_page = MetaInspector.new(root_url)
-        doc = Nokogiri::XML(open(root_url))
-        begin
-          name = root_page.title[0..10] || "#{doc.xpath("//title").children.first.text[0..10]} Feed"
-        rescue
-          name = "#{root_page.host[0..10]} Feed"
-        end
-        logo = root_page.images.favicon
-        description = root_page.description
-      else
-        @success = false
-        return
-      end
-      @website = Website.create!({name: name,
-                                  url: url,
-                                  description: description,
-                                  logo: logo})
-      UserWebsite.create!({user_id: @user_id, website_id: @website.id})
+    feed = nil
+    @website = Website.find_by_url(@url)
+    if @website
+      puts "ALREADY CREATED"
+      UserWebsite.find_or_create_by(user_id: @user_id, website_id: @website.id)
+      @success = true
+      return
     else
-      @success = false
+      begin
+        open(@url) do |rss|
+          feed = RSS::Parser.parse(rss)
+        end
+        name = feed.channel.title
+        description = feed.channel.description
+        root_uri = URI(feed.channel.link)
+        root_url = "#{root_uri.scheme}://#{root_uri.host}"
+        root_page = MetaInspector.new(root_url, :allow_non_html_content => true, download_images: false)
+        logo = root_page.images.favicon
+        @website = Website.create!({name: name,
+                                    url: @url,
+                                    description: description,
+                                    logo: logo})
+        UserWebsite.create!({user_id: @user_id, website_id: @website.id})
+        @success = true;
+      rescue Exception => e
+        puts e.message
+        @success = false
+      end
     end
   end
-
-  private
-
-  def feed_validation(page)
-    begin
-      feed = MetaInspector.new(page.feed)
-    rescue
-      feed = page
-    end
-    return (feed.content_type == ("text/xml" || "application/rss+xml") ? true : false)
-  end
-
 end
